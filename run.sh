460ca9c0f7d90663f52e3cfc35d7ce6a1be768f8
Ciprian T <ciprian@mediamining.univ-lyon2.fr>
2016-06-13 17:05:22 +0200

    TM tests

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ PyCC="python"
 
 
 
-for i in `seq 1 6`
+for i in `seq 1 1`
 do
     $PyCC tm_all_default.py $FILE_NAME_CONF_CLEAN $NUM_TOPICS_NEWS $NUM_ITER $NUM_CORES > $OUTPUT_FILE"news_cleanText_"$i
 done;
